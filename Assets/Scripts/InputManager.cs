43cee883203b4c66bc5e52d60137245e6744de30
Rahul Barate <baraterahul48@gmail.com>
2026-05-12 00:16:54 +0530

    Create and export Rat FBX. Create and setup Rat prefab and animation. Rename InputActions file. Add InputManger with singleton pattern. Add RatController script with basic movement and jump. Update gitignore to ignore FBX folder

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public InputActions inputActions;
+    public static InputManager Instance;
+    void Awake()
+    {
+        if (Instance != this && Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        inputActions = new InputActions();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+
+}
